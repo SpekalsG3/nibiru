f6ad21269fd9b1b0cc1f7a94bf3ea393b1f33e08
SpekalsG3 <karpick.kirill@gmail.com>
2024-09-10 19:39:28 +0300

    fix(deps): fix context in upgrade handlers

--- app/upgrades/v1_1_0/constants.go
+++ app/upgrades/v1_1_0/constants.go
@@ -1,10 +1,11 @@
 package v1_1_0
 
 import (
+	"context"
+
 	"cosmossdk.io/store/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
 
 	"github.com/NibiruChain/nibiru/v2/app/upgrades"
 	inflationtypes "github.com/NibiruChain/nibiru/v2/x/inflation/types"
@@ -15,7 +16,7 @@ const UpgradeName = "v1.1.0"
 var Upgrade = upgrades.Upgrade{
 	UpgradeName: UpgradeName,
 	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator) upgradetypes.UpgradeHandler {
-		return func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+		return func(ctx context.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 			return mm.RunMigrations(ctx, cfg, fromVM)
 		}
 	},
